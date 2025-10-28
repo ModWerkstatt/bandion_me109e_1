27b950824fb68c458ccbcb444131729dd14cd090
Florian Thiel <flth@flth.de>
2025-10-28 20:59:14 +0100

    Initial

---
+++ mod.lua
@@ -0,0 +1,20 @@
+function data()
+return {
+info = {
+  minorVersion = 0,
+  severityAdd = "NONE",
+  severityRemove = "CRITICAL",
+  name = _("mod_name"),
+  description = _("mod_desc"),
+  authors = {
+        {
+            name = "Bandion",
+            role = "CREATOR",
+        },
+  },
+  tags = {"europe", "plane", },
+  		visible = true,
+},
+
+ }
+end
---
+++ strings.lua
@@ -0,0 +1,33 @@
+function data()
+return {
+
+		de = {
+
+			["mod_name"] = "Messerschmitt BF 109 E",
+			["mod_desc"] = "Mit der F-Version erreichte die Bf 109 in den Augen vieler ihrer Piloten den Zenit ihrer Leistungsfähigkeit. Spätere Versionen verfügten unter ständig zunehmender Tragflächenbelastung zwar über stärkere Motoren, Bewaffnungsvarianten und höhere Einsatzmassen, doch die Abstimmung zwischen der Zelle und dem Motor verliehen der Bf 109 F die besten Flug- und Steuereigenschaften aller Varianten der Bf 109.",
+
+            ["me109f_menu"] = "Messerschmitt BF 109 E",
+			["me109f_1_name"] = "BF 109 F JG51",
+			["me109f_2_name"] = "BF 109 F JG1",
+            ["me109f_3_name"] = "BF 109 F JG2",
+            ["me109f_4_name"] = "BF 109 F JG51 \"Komet\"",
+			["me109f_desc"] = "Mit der F-Version erreichte die Bf 109 in den Augen vieler ihrer Piloten den Zenit ihrer Leistungsfähigkeit. Spätere Versionen verfügten unter ständig zunehmender Tragflächenbelastung zwar über stärkere Motoren, Bewaffnungsvarianten und höhere Einsatzmassen, doch die Abstimmung zwischen der Zelle und dem Motor verliehen der Bf 109 F die besten Flug- und Steuereigenschaften aller Varianten der Bf 109.",
+
+		},
+
+		en = {
+
+			["mod_name"] = "Messerschmitt BF 109 E",
+			["mod_desc"] = "With the F version, the Bf 109 reached the zenith of its performance in the eyes of many of its pilots. Later versions had more powerful engines, armament variants and higher operational masses with constantly increasing wing loading, but the coordination between the airframe and the engine gave the Bf 109 F the best flight and control properties of all variants of the Bf 109.",
+
+			["me109f_menu"] = "Messerschmitt BF 109 E",
+            ["me109f_1_name"] = "BF 109 F JG51",
+			["me109f_2_name"] = "BF 109 F JG1",
+			["me109f_3_name"] = "BF 109 F JG2",
+			["me109f_4_name"] = "BF 109 F JG51 \"Komet\"",
+			["me109f_4_desc"] = "With the F version, the Bf 109 reached the zenith of its performance in the eyes of many of its pilots. Later versions had more powerful engines, armament variants and higher operational masses with constantly increasing wing loading, but the coordination between the airframe and the engine gave the Bf 109 F the best flight and control properties of all variants of the Bf 109.",
+
+		},
+
+	}
+end
