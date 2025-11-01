cd989bd5c8e40223ce2151572ae5ac5f34f5b8c3
Bandion-MW <norman.kroppen@googlemail.com>
2025-11-01 22:38:53 +0100

    Fast fertig. Animation noch und Cblend ggf

---
+++ res/config/sound_set/aircraft_me109e.lua
@@ -0,0 +1,23 @@
+local soundsetutil = require "soundsetutil"
+
+function data()
+
+local data = soundsetutil.makeSoundSet()
+
+soundsetutil.addTrackParam01(data, "vehicle/aircraft_me109/_aircraft_prop_109_idle.wav", 25.0,
+		{ { 0.0, 0.5 }, { 0.5, 1.0 }, { 1.0, 0.15 } },
+		{ { 0.05, 0.8 }, { 1.0, 1.5 } }, "power01")
+		
+soundsetutil.addTrackParam01(data, "vehicle/aircraft_me109/_aircraft_prop_109_power.wav", 25.0,
+		{ { 0.3, 0.0 }, { 0.6, 0.9 }, { 1.0, 1.0 } },
+		{ { 0.3, 0.8 }, { 0.6, 0.8 }, { 1.0, 1.1 } }, "power01")
+		
+soundsetutil.addTrackParam01(data, "vehicle/aircraft_me109/_aircraft_prop_109_distant.wav", 100.0,
+		{ { 0.5, 0.0 }, { 0.9, 0.3 } },
+		{ { 0, 1.0 } }, "power01")
+
+soundsetutil.addEvent(data, "land", { "vehicle/aircraft_me109/_aircraft_prop_old_land.wav" }, 25.0)
+
+return data
+
+end
