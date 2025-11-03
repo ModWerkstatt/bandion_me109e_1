2c8c39d655de515171998788b85e014b342e4eb6
Florian Thiel <flth@flth.de>
2025-11-03 23:20:55 +0100

    Strings eingetragen

--- strings.lua
+++ strings.lua
@@ -4,28 +4,42 @@ return {
 		de = {
 
 			["mod_name"] = "Messerschmitt BF 109 E",
-			["mod_desc"] = "Mit der F-Version erreichte die Bf 109 in den Augen vieler ihrer Piloten den Zenit ihrer Leistungsfähigkeit. Spätere Versionen verfügten unter ständig zunehmender Tragflächenbelastung zwar über stärkere Motoren, Bewaffnungsvarianten und höhere Einsatzmassen, doch die Abstimmung zwischen der Zelle und dem Motor verliehen der Bf 109 F die besten Flug- und Steuereigenschaften aller Varianten der Bf 109.",
+			["mod_desc"] = "Im Januar 1939 wurde die Produktion der Bf 109 auf die neue Version E mit einem neuen Motor umgestellt. Die Bf 109 E erwies sich als äußerst gutes Jagdflugzeug und stellte den überwiegeden Teil der deutschen Jagdeinheiten, auch wenn ihre Reichweite begrenzt war. Es gab viele Varianten und Lackierungen.",
 
-            ["me109f_menu"] = "Messerschmitt BF 109 E",
-			["me109f_1_name"] = "BF 109 F JG51",
-			["me109f_2_name"] = "BF 109 F JG1",
-            ["me109f_3_name"] = "BF 109 F JG2",
-            ["me109f_4_name"] = "BF 109 F JG51 \"Komet\"",
-			["me109f_desc"] = "Mit der F-Version erreichte die Bf 109 in den Augen vieler ihrer Piloten den Zenit ihrer Leistungsfähigkeit. Spätere Versionen verfügten unter ständig zunehmender Tragflächenbelastung zwar über stärkere Motoren, Bewaffnungsvarianten und höhere Einsatzmassen, doch die Abstimmung zwischen der Zelle und dem Motor verliehen der Bf 109 F die besten Flug- und Steuereigenschaften aller Varianten der Bf 109.",
+            ["me109e_menu"] = "Messerschmitt BF 109 E",
+			["me109e_1_name"] = "BF 109 E feldgrün (1939)",
+			["me109e_2_name"] = "BF 109 E JG26 (1939)",
+            ["me109e_3_name"] = "BF 109 E Nachtjäger (1939)",
+            ["me109e_4_name"] = "BF 109 E JG2 (1939)",
+            ["me109e_5_name"] = "BF 109 E JG27 (1939)",
+            ["me109e_6_name"] = "BF 109 E JG27 Trop (1939)",
+            ["me109e_7_name"] = "BF 109 E JG27 Flecktrop (1939)",
+            ["me109e_8_name"] = "BF 109 E Frankreich (1939)",
+            ["me109e_9_name"] = "BF 109 E England (1939)",
+            ["me109e_10_name"] = "BF 109 E JG53 (1939)",
+            ["me109e_11_name"] = "BF 109 E Polen (1939)",
+			["me109e_desc"] = "Im Januar 1939 wurde die Produktion der Bf 109 auf die neue Version E mit einem neuen Motor umgestellt. Die Bf 109 E erwies sich als äußerst gutes Jagdflugzeug und stellte den überwiegeden Teil der deutschen Jagdeinheiten, auch wenn ihre Reichweite begrenzt war. Es gab viele Varianten und Lackierungen.",
 
 		},
 
 		en = {
 
 			["mod_name"] = "Messerschmitt BF 109 E",
-			["mod_desc"] = "With the F version, the Bf 109 reached the zenith of its performance in the eyes of many of its pilots. Later versions had more powerful engines, armament variants and higher operational masses with constantly increasing wing loading, but the coordination between the airframe and the engine gave the Bf 109 F the best flight and control properties of all variants of the Bf 109.",
+			["mod_desc"] = "In January 1939, production of the Bf 109 was switched to the new E version with a new engine. The Bf 109 E proved to be an extremely good fighter aircraft and made up the majority of German fighter units, even though its range was limited. There were many variants and paint schemes.",
 
-			["me109f_menu"] = "Messerschmitt BF 109 E",
-            ["me109f_1_name"] = "BF 109 F JG51",
-			["me109f_2_name"] = "BF 109 F JG1",
-			["me109f_3_name"] = "BF 109 F JG2",
-			["me109f_4_name"] = "BF 109 F JG51 \"Komet\"",
-			["me109f_4_desc"] = "With the F version, the Bf 109 reached the zenith of its performance in the eyes of many of its pilots. Later versions had more powerful engines, armament variants and higher operational masses with constantly increasing wing loading, but the coordination between the airframe and the engine gave the Bf 109 F the best flight and control properties of all variants of the Bf 109.",
+			["me109e_menu"] = "Messerschmitt BF 109 E",
+            ["me109e_1_name"] = "BF 109 E field green (1939)",
+			["me109e_2_name"] = "BF 109 E JG25 (1939)",
+			["me109e_3_name"] = "BF 109 E night hunter (1939)",
+			["me109e_4_name"] = "BF 109 E JG2 (1939)",
+            ["me109e_5_name"] = "BF 109 E JG27 (1939)",
+            ["me109e_6_name"] = "BF 109 E JG27 Trop (1939)",
+            ["me109e_7_name"] = "BF 109 E JG27 Spot trop (1939)",
+            ["me109e_8_name"] = "BF 109 E France (1939)",
+            ["me109e_9_name"] = "BF 109 E England (1939)",
+            ["me109e_10_name"] = "BF 109 E JG53 (1939)",
+            ["me109e_11_name"] = "BF 109 E Poland (1939)",
+			["me109e_desc"] = "In January 1939, production of the Bf 109 was switched to the new E version with a new engine. The Bf 109 E proved to be an extremely good fighter aircraft and made up the majority of German fighter units, even though its range was limited. There were many variants and paint schemes.",
 
 		},
 
